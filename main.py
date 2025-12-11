53ab23e168b2a69cc10f279dcf30fc6ef662a07a
dineshmurani <51493204+dineshmurani@users.noreply.github.com>
2025-12-11 11:11:57 -0800

    S24 223 Agent Executing Tools(Python Functions)

--- main.py
+++ main.py
@@ -2,14 +2,23 @@ from dotenv import load_dotenv
 import os
 from langchain_core.messages import HumanMessage, AIMessage
 from langchain_core.output_parsers import StrOutputParser
-from langchain_core.prompts import ChatPromptTemplate
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
 from langchain_google_genai import ChatGoogleGenerativeAI
-
+from langchain.tools import tool
+from langchain.agents import create_openai_tools_agent, AgentExecutor
 
 load_dotenv()
 
 todoist_api_key = os.getenv("TODOIST_API_KEY")
 gemini_api_key = os.getenv("GEMINI_API_KEY")
+
+@tool
+def add_task():
+    """Add a new task to the user's task list. Use this when the user wants to add or create a task"""
+    print("Adding a task")
+    print("task added")
+
+tools = [add_task]
 
 llm = ChatGoogleGenerativeAI(
     model='gemini-2.5-flash',
@@ -18,15 +27,20 @@ llm = ChatGoogleGenerativeAI(
 )
 
 system_prompt = "You are a helpful assistant. You will help the user add tasks."
-user_input = "Tell me the best way to manage my time?"
+user_input = "add task to buy some milk from the local shop"
 
 prompt = ChatPromptTemplate([
     ("system", system_prompt),
-    ("user", user_input)
+    ("user", user_input),
+    MessagesPlaceholder("agent_scratchpad")
 ])
 
-chain = prompt | llm | StrOutputParser()
+# chain = prompt | llm | StrOutputParser()
+agent = create_openai_tools_agent(llm, tools, prompt)
+agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
 
-response = chain.invoke({"input":user_input})
 
-print(response)
+# response = chain.invoke({"input":user_input})
+response = agent_executor.invoke({"input": user_input})
+
+print(response['output'])
