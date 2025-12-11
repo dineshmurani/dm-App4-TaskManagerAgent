53736edeb05d54c25393ad516b0f9f08dd33f23b
dineshmurani <51493204+dineshmurani@users.noreply.github.com>
2025-12-11 10:35:19 -0800

    S24 222 Getting AI Responses from Langchain

--- main.py
+++ main.py
@@ -1,11 +1,32 @@
 from dotenv import load_dotenv
 import os
+from langchain_core.messages import HumanMessage, AIMessage
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_google_genai import ChatGoogleGenerativeAI
+
 
 load_dotenv()
 
 todoist_api_key = os.getenv("TODOIST_API_KEY")
 gemini_api_key = os.getenv("GEMINI_API_KEY")
 
-print(todoist_api_key)
-print(gemini_api_key)
+llm = ChatGoogleGenerativeAI(
+    model='gemini-2.5-flash',
+    google_api_key=gemini_api_key,
+    temperature=0.3
+)
 
+system_prompt = "You are a helpful assistant. You will help the user add tasks."
+user_input = "Tell me the best way to manage my time?"
+
+prompt = ChatPromptTemplate([
+    ("system", system_prompt),
+    ("user", user_input)
+])
+
+chain = prompt | llm | StrOutputParser()
+
+response = chain.invoke({"input":user_input})
+
+print(response)
